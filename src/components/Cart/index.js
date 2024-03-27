9ebb8b495c7379d1928095639bba9f947bda81a0
B Vamshi Krishna <147190434+vamshi-krishna-7@users.noreply.github.com>
2024-03-27 07:35:23 +0530

    Update index.js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom'
+
 import Header from '../Header'
 import './index.css'
 
@@ -6,10 +8,15 @@ const Cart = () => (
     <Header />
     <div className="cart-container">
       <img
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-empty-cart-img.png"
         alt="cart"
         className="cart-img"
       />
+      <Link to="/products">
+        <button type="button" className="button">
+          Shop Now
+        </button>
+      </Link>
     </div>
   </>
 )
